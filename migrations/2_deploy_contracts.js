bbf41abaa16e2d39415b8f7ee76164e7e14633d8
Lorran Sutter <sutterlorran@gmail.com>
2020-07-26 11:07:07 -0400

    Install eslint

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 const BN = require('bn.js');
 
+/* eslint-disable-next-line no-undef */
 const Token = artifacts.require('Token');
+/* eslint-disable-next-line no-undef */
 const AirDrop = artifacts.require('AirDrop');
 
 module.exports = async function (deployer, network, accounts) {
-    const merkleRoot = null;
+	const merkleRoot = null;
 
-    await deployer.deploy(Token, 'Token', 'TKN', new BN(1e42), { from: accounts[0] });
-    await deployer.deploy(AirDrop, merkleRoot, Token.address, { from: accounts[0] });
+	await deployer.deploy(Token, 'Token', 'TKN', new BN(1e42), { from: accounts[0] });
+	await deployer.deploy(AirDrop, merkleRoot, Token.address, { from: accounts[0] });
 };
